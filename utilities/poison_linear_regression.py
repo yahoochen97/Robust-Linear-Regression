30042632357c8977c292001279a287515fc8bf3e
Hanyang Liu <ericstarkhan@gmail.com>
2020-02-28 12:19:18 -0600

    poisoning attack on LR

--- utilities/poison_linear_regression.py
+++ utilities/poison_linear_regression.py
@@ -2,13 +2,13 @@ import numpy as np
 
 
 def poison_linear_regression(X, y, X_c, y_c, lam, betta, sigma, eps):
-    '''
+    """
     Generate additional adversarial training data.
     :param X, y: the training data and lables
     :param X_c: set of initial data vectors x_c - q by k
     :param y_c: set of initial data labels - q by 1
     :return: final attack points
-    '''
+    """
     max_iter = 100
     for iter in range(max_iter):
         X_new = np.concatenate([X, X_c], axis=0)
@@ -17,7 +17,7 @@ def poison_linear_regression(X, y, X_c, y_c, lam, betta, sigma, eps):
         X_c_old = X_c
         for ind, x_c in enumerate(X_c):
             # learn classifier w and b on new data X_new, y_new
-            delta_W = compute_subgradient(X_new, y_new, x_c, w, b)
+            delta_W = compute_gradient(X_new, y_new, x_c, y_c[ind], w, b, lam)
             d = box_projection(x_c + delta_W, 0, 1) - x_c
             # line search to set the gradient step etta
             for k in range(10):
@@ -26,31 +26,54 @@ def poison_linear_regression(X, y, X_c, y_c, lam, betta, sigma, eps):
                 # update x_c along direction d to maximize error
                 x_c = x_c + etta * d
                 X_c[ind] = x_c
-                if attack_objective(x_c, y_c[ind], w) <= (
-                        attack_objective(x_c_old, y_c[ind], w) - sigma * etta * np.linalg.norm(d, 2)):
+                if attack_objective(x_c, y_c[ind], w, b, lam) <= (
+                        attack_objective(x_c_old, y_c[ind], w, b, lam) - sigma * etta * np.linalg.norm(d, 2)):
                     break
-        if eps < attack_objective(X_c, y_c, w) - attack_objective(X_c_old, y_c, w) < eps:
+        if eps < attack_objective(X_c, y_c, w, b, lam) - attack_objective(X_c_old, y_c, w, b, lam) < eps:
             break
 
     return X_c
 
 
 def learn_LRclassifier(X, y, lam):
-    '''
+    """
     Given data and labels, output the linear regression classifier
-    :param X:
+    :param X_tl:
     :param y:
     :param lamda:
     :return:
-    '''
+    """
+    X_tl = np.concatenate([X, np.ones(len(y), 1)], axis=0)
+    w_tl = np.linalg.inv(X_tl.T.dot(X_tl)).dot(X_tl.T).dot(y)
+    w = w_tl[0:-1]
+    b = w_tl[-1]
 
     return w, b
 
 
-def compute_subgradient(X, y, x_c, w, b):
+def compute_gradient(X, y, x_c, y_c, w, b, lam):
 
+    """
+    compute gradient of w and b w.r.t. x_c
+    """
+    n, d = np.shape(X)
+    Sig = 1/n * X.T.dot(X)  # shape: d by d
+    mu = 2/n * X.T.dot(np.ones(n))  # shape: d by 1
+    M = x_c.dot(w.T) + (w.T.dot(x_c) + b - y_c) * np.identity(d)  # shape: d by d
+    v = np.identity(d)
 
-    return subgradient
+    # solve linear system to obtain
+    A = np.concatenate([np.concatenate([Sig + lam * v, mu], axis=1), np.concatenate([mu.T, 1], axis=1)], axis=0)
+    B = -1/n * np.concatenate([M, w.T], axis=0)
+    solution_vec = np.linalg.solve(A, B)
+    dw = solution_vec[0:d]
+    db = solution_vec[-1]
+
+    # compute gradient
+    r = w.T  # r is the gradient of regularization term w.r.t. w, for regression it is w^T
+    grad = 1/n * (X.dot(w) + b * np.ones(n) - y).T.dot(X.dot(dw) + db.dot(np.ones(n))) + lam * r.dot(dw)
+
+    return grad
 
 
 def box_projection(x, lowerbound, upperbound):
@@ -58,6 +81,10 @@ def box_projection(x, lowerbound, upperbound):
     return x_projected
 
 
-def attack_objective(X, y, w):
+def attack_objective(X, y, w, b, lam):
 
-    return objective_value
+    n, d = np.shape(X)
+    obj_value = 1/n * np.linalg.norm(X.dot(w) + b * np.ones(n) - y, 2) + 1/2 * lam * w.T.dot(w)
+
+    return obj_value
+
