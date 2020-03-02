08b854d3b14356b08fe71bb7ff502fd257575ee9
Hanyang Liu <ericstarkhan@gmail.com>
2020-03-02 00:26:16 -0600

    debug adversary

--- main.py
+++ main.py
@@ -7,9 +7,21 @@ def main():
     n = 350
     m = 50
     n1 = 50
-    for k in range(20):
-        X_star = generate_pristine_data(n, k, m)
-        X_A = poison_subspace_recovery(X_star, n1, k, m)
+    # for k in range(20):
+    #     X_star = generate_pristine_data(n, k, m)
+    #     X_A = poison_subspace_recovery(X_star, n1, k, m)
+
+    k = 20
+    X = np.random.rand(n, k)
+    w = np.random.rand(k)
+    y = X.dot(w)
+    X_c = X[0:50, :]
+    y_c = y[0:50]
+    lam = 0.001
+    betta = 0.5
+    sigma = 0.01
+    eps = 0.01
+    X_a = poison_linear_regression(X, y, X_c, y_c, lam, betta, sigma, eps)
 
 if __name__ == "__main__":
     main()
--- utilities/poison_linear_regression.py
+++ utilities/poison_linear_regression.py
@@ -8,6 +8,7 @@ def poison_linear_regression(X, y, X_c, y_c, lam, betta, sigma, eps):
     :param X_c: set of initial data vectors x_c - q by k
     :param y_c: set of initial data labels - q by 1
     :param lam: regularization coefficient for the linear classifier
+    :param betta, sigma, eps: betta in (0, 1), sigma and eps are small positive contants
     :return: final attack points
     """
     max_iter = 100
@@ -23,7 +24,7 @@ def poison_linear_regression(X, y, X_c, y_c, lam, betta, sigma, eps):
             # line search to set the proper gradient step etta
             x_c_old = x_c
             for k in range(10):
-                etta = betta ^ k
+                etta = pow(betta, k + 1)
                 # update x_c along direction d to maximize error
                 x_c = x_c_old + etta * d
                 if attack_objective(x_c, y_c[ind], w, b, lam) <= (
@@ -45,8 +46,8 @@ def learn_LRclassifier(X, y, lam):
     :return:
     """
     n, d = np.shape(X)
-    X_tl = np.concatenate([X, np.ones(n, 1)], axis=0)
-    w_tl = np.linalg.inv(X_tl.T.dot(X_tl) + lam * np.ones(d)).dot(X_tl.T).dot(y)
+    X_tl = np.concatenate([X, np.ones([n, 1])], axis=1)
+    w_tl = np.linalg.inv(X_tl.T.dot(X_tl) + lam * np.identity(d + 1)).dot(X_tl.T).dot(y)
     w = w_tl[0:-1]
     b = w_tl[-1]
 
@@ -58,21 +59,22 @@ def compute_gradient(X, y, x_c, y_c, w, b, lam):
     """compute gradient of w and b w.r.t. x_c"""
     n, d = np.shape(X)
     Sig = 1/n * X.T.dot(X)  # shape: d by d
-    mu = 2/n * X.T.dot(np.ones(n))  # shape: d by 1
+    mu = 2/n * X.T.dot(np.ones([n, 1]))  # shape: d by 1
     M = x_c.dot(w.T) + (w.T.dot(x_c) + b - y_c) * np.identity(d)  # shape: d by d
     v = np.identity(d)
 
     # solve linear system to obtain
-    A = np.concatenate([np.concatenate([Sig + lam * v, mu], axis=1), np.concatenate([mu.T, 1], axis=1)], axis=0)
-    B = -1/n * np.concatenate([M, w.T], axis=0)
+    A = np.concatenate([np.concatenate([Sig + lam * v, mu.reshape(d, 1)], axis=1), np.concatenate([mu.reshape(d, 1).T, np.ones([1, 1])], axis=1)], axis=0)
+    B = -1/n * np.concatenate([M, w.reshape(d, 1).T], axis=0)
     solution_vec = np.linalg.solve(A, B)
     dw = solution_vec[0:d]
-    db = solution_vec[-1]
+    db = solution_vec[-1].reshape(1, d)
 
     """compute gradient"""
     # compute gradient
-    r = w.T  # r is the gradient of regularization term w.r.t. w, for regression it is w^T
-    grad = 1/n * (X.dot(w) + b * np.ones(n) - y).T.dot(X.dot(dw) + db.dot(np.ones(n))) + lam * r.dot(dw)
+    r = w.reshape(d, 1).T  # r is the gradient of regularization term w.r.t. w, for regression it is w^T
+    grad = (X.dot(w) + b * np.ones([n, 1]) - y).T.dot(X.dot(dw) + np.ones([n, 1]).dot(db)) + lam * r.dot(dw)
+    grad = np.mean(grad, axis=0)
 
     return grad
 
@@ -89,6 +91,8 @@ def box_projection(x, low, upp):
 
 def attack_objective(X, y, w, b, lam):
 
+    if np.ndim(X) == 1:
+        X = X.reshape(1, len(X))
     n, d = np.shape(X)
     obj_value = 1/n * np.linalg.norm(X.dot(w) + b * np.ones(n) - y, 2) + 1/2 * lam * w.T.dot(w)
 
