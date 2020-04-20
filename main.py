197666c1761f829dffddd42bb6af177aeb0851ba
HanyangLiu <hanyang.liu@wustl.edu>
2020-04-20 13:02:04 -0500

    robust regression

--- main.py
+++ main.py
@@ -106,79 +106,90 @@ def recovery():
 
 def regression():
 
+    runs = 10
     m = 20
     k = 20
     n1s = [10 + 10 * i for i in range(20)]
-    RMSEs_rlr = []
-    RMSEs_lr = []
-    RMSEs_lro = []
+    RMSEs_rlr = np.zeros([len(n1s), runs])
+    RMSEs_lr = np.zeros([len(n1s), runs])
+    RMSEs_lro = np.zeros([len(n1s), runs])
 
-    for n1 in n1s:
+    for idx, n1 in enumerate(n1s):
 
         n = 400 - n1  # number of pristine data
 
-        # generate training dataset
-        X_star, _ = generate_pristine_data(n, k, m, iters=1000)
-        scalar = MinMaxScaler()
-        scalar.fit(X_star)
-        feature_range = scalar.feature_range
-        X_star = scalar.transform(X_star)
-        X_noised = X_star + np.random.normal(loc=0, scale=0.01, size=X_star.shape)
-        w_star = np.random.rand(m)
-        y_star = X_star.dot(w_star)
+        for run in range(runs):
+            # generate training dataset
+            X_star, _ = generate_pristine_data(n, k, m, iters=1000)
+            scalar = MinMaxScaler()
+            scalar.fit(X_star)
+            feature_range = scalar.feature_range
+            X_star = scalar.transform(X_star)
+            X_noised = X_star + np.random.normal(loc=0, scale=0.01, size=X_star.shape)
+            w_star = np.random.rand(m)
+            y_star = X_star.dot(w_star)
 
-        # randomly generate poisoning seeds
-        ind_adv_seeds = np.random.choice(n, n1, replace=False)
-        X_c = X_star[ind_adv_seeds]
-        y_c = y_star[ind_adv_seeds]
-        X_clean = np.concatenate([X_noised, X_c], axis=0)
-        y_clean = np.concatenate([y_star, y_c], axis=0)
+            # randomly generate poisoning seeds
+            ind_adv_seeds = np.random.choice(n, n1, replace=False)
+            X_c = X_star[ind_adv_seeds]
+            y_c = y_star[ind_adv_seeds]
+            X_clean = np.concatenate([X_noised, X_c], axis=0)
+            y_clean = np.concatenate([y_star, y_c], axis=0)
 
-        # poison training data
-        param = dict()
-        param['lam'] = 0.001
-        param['beta'] = 0.5
-        param['sigma'] = 1e-4
-        param['eps'] = 1e-6
-        X_a2 = poison_linear_regression(X_star, y_star, feature_range, X_c, y_c, param, max_iter=2000)
-        X_all = np.concatenate([X_noised, X_a2], axis=0)
-        y_all = np.concatenate([y_star, y_c], axis=0)
+            # poison training data
+            param = dict()
+            param['lam'] = 0.001
+            param['beta'] = 0.5
+            param['sigma'] = 1e-4
+            param['eps'] = 1e-6
+            X_a2 = poison_linear_regression(X_star, y_star, feature_range, X_c, y_c, param, max_iter=2000)
+            X_all = np.concatenate([X_noised, X_a2], axis=0)
+            y_all = np.concatenate([y_star, y_c], axis=0)
 
-        # train robust regression model
-        robust_regression = RobustRegression(X_all, y_all, n, n1, k, max_iters=100)
-        w_predict = robust_regression.trimmed_principal_component_regression()
+            # train robust regression model
+            robust_regression = RobustRegression(X_all, y_all, n, n1, k, max_iters=100)
+            w_predict = robust_regression.trimmed_principal_component_regression()
 
-        # train normal LR classifier
-        w_lr, b_lr = train_classifier(X_all, y_all, param['lam'])
-        w_lro, b_lro = train_classifier(X_clean, y_clean, param['lam'])
+            # train normal LR classifier
+            w_lr, b_lr = train_classifier(X_all, y_all, param['lam'])
+            w_lro, b_lro = train_classifier(X_clean, y_clean, param['lam'])
 
-        # generate test dataset
-        X_test, _ = generate_pristine_data(n, k, m, iters=1000)
-        X_test = scalar.transform(X_test)
-        X_test = X_test + np.random.normal(loc=0, scale=0.01, size=X_star.shape)
-        y_test = X_test.dot(w_star)
+            # generate test dataset
+            X_test, _ = generate_pristine_data(n, k, m, iters=1000)
+            X_test = scalar.transform(X_test)
+            X_test = X_test + np.random.normal(loc=0, scale=0.01, size=X_star.shape)
+            y_test = X_test.dot(w_star)
 
-        # test robust regression model
-        y_predict = X_test.dot(w_predict)
-        RMSE_rlr = np.sqrt(np.mean((y_predict - y_test) ** 2))
-        RMSEs_rlr.append(RMSE_rlr)
+            # test robust regression model
+            y_predict = X_test.dot(w_predict)
+            RMSE_rlr = np.sqrt(np.mean((y_predict - y_test) ** 2))
+            RMSEs_rlr[idx, run] = RMSE_rlr
 
-        # test normal LR model
-        y_lr = X_test.dot(w_lr) + b_lr
-        RMSE_lr = np.sqrt(np.mean((y_lr - y_test) ** 2))
-        RMSEs_lr.append(RMSE_lr)
-        y_lro = X_test.dot(w_lro) + b_lro
-        RMSE_lro = np.sqrt(np.mean((y_lro - y_test) ** 2))
-        RMSEs_lro.append(RMSE_lro)
-        print('# corrupted rows', n1, 'RMSE_rlr:', RMSE_rlr, 'RMSE_lr:', RMSE_lr, 'RMSE_lro', RMSE_lro)
+            # test normal LR model
+            y_lr = X_test.dot(w_lr) + b_lr
+            RMSE_lr = np.sqrt(np.mean((y_lr - y_test) ** 2))
+            RMSEs_lr[idx, run] = RMSE_lr
+            y_lro = X_test.dot(w_lro) + b_lro
+            RMSE_lro = np.sqrt(np.mean((y_lro - y_test) ** 2))
+            RMSEs_lro[idx, run] = RMSE_lro
 
+        print('# corrupted rows', n1,
+              'RMSE_rlr:', np.median(RMSEs_rlr[idx, :]),
+              'RMSE_lr:', np.median(RMSEs_lr[idx, :]),
+              'RMSE_lro', np.median(RMSEs_lro[idx, :]))
+
+    np.savetxt('test.out', (RMSEs_rlr, RMSEs_lr, RMSEs_lro), delimiter=',')
     # plot RMSE_rlr
     plt.figure()
-    plt.plot(n1s, RMSEs_rlr)
-    plt.title('Regression Output RMSE_rlr vs # corrupted rows')
+    plt.plot(n1s, np.median(RMSEs_rlr, axis=1), label='Robust LR')
+    plt.plot(n1s, np.median(RMSEs_lr, axis=1), label='LR')
+    plt.plot(n1s, np.median(RMSEs_lro, axis=1), label='LR attack free')
+    plt.title('Regression Output RMSE vs # corrupted rows')
     plt.xlabel('corrupted rows')
-    plt.ylabel('RMSE_rlr')
-    plt.ylim(0, 1.2)
+    plt.ylabel('RMSE')
+    plt.legend()
+    plt.show()
+    plt.savefig('RMSE.png')
 
 
 if __name__ == "__main__":
